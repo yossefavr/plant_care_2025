926157a1899f1ff64d62d66e1580315161f2dcd1
LiavKaykov <liavkaykov@gmail.com>
2025-02-27 14:55:52 +0200

    adding a list of events page, by liav and itay

---
+++ src/main/java/com/plant_care_2025/repository/LiavAndItayRepository.java
@@ -0,0 +1,10 @@
+package com.plant_care_2025.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.plant_care_2025.model.LiavAndItay;
+
+
+public interface LiavAndItayRepository extends JpaRepository<LiavAndItay, Long> {
+}
